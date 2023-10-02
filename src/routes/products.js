b95905d8dab64d2528601655039aa871c588e13e
curyAilen <cury.ailena@gmail.com>
2023-10-01 21:32:05 -0300

    Rutas con respecto a productos estan listas

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,17 +3,6 @@ import ProductManager from "../ProductManager.js";
 import { readFileSync } from "fs";
 import path from "path";
 import __dirname from "../utils.js";
-import multer from "multer";
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/img");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-const upload = multer({ storage });
 const router = express.Router();
 const pathProducts = path.join(__dirname, "../products.json");
 const fileProducts = JSON.parse(readFileSync(pathProducts));
@@ -51,26 +40,48 @@ router.get("/:pid", (req, res) => {
       res.status(404).json({ error: "Producto no encontrado" });
     }
   } catch (error) {
-    res.status(500).json({ error: "Error interno del servidor" });
+    res.status(500).json({ error: "No se ah encontrado el producto indicado" });
+  }
+});
+router.get("/edit/:pid", (req, res) =>{
+  const productId = parseInt(req.params.pid);
+  try {
+    productManager.getProduct();
+    const product = productManager.getProductById(productId);
+
+    if (product) {
+      res.render("editProduct", {
+        title: "Detalle producto",
+        product: product,
+      });
+    } else {
+      res.status(404).json({ error: "Producto no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "No se ah encontrado el producto indicado" });
+  }
+})
+router.put("/edit/:pid", (req, res) => {
+  const productId = parseInt(req.params.pid);
+  const updateData = req.body;
+
+  try {
+    const updatedProduct = productManager.updateProduct(productId, updateData);
+    res.redirect(`/api/products/${productId}`);  
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 });
 
-router.get("/create", (req, res) => {
-    res.render("newProduct", { title: "Nuevo Producto" });
-  });
-  
+router.delete("/delete/:pid", (req, res) => {
+  const productId = parseInt(req.params.pid);
 
-  router.post("/", upload.single("filename"), (req, res) => {
-  
-  });
-  
-
-router.put("/:pid", (req, res) => {
-  // Implementación de la ruta para actualizar un producto
-});
-
-router.delete("/:pid", (req, res) => {
-  // Implementación de la ruta para eliminar un producto
+  try {
+    productManager.deleteProduct(productId);
+    res.json({ message: 'Producto eliminado exitosamente', productId });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 export default router;
